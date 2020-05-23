b81492b519558faaa351cb3b3709abad2802704c
PRO8L3M <230440@student.pwr.edu.pl>
2020-05-23 15:00:45 +0200

    changed: removed spacing

--- app/src/main/java/com/opus/repository/LoginRepositoryImpl.kt
+++ app/src/main/java/com/opus/repository/LoginRepositoryImpl.kt
@@ -9,8 +9,6 @@ import com.opus.data.entity.safeCall
 class LoginRepositoryImpl(private val local: LocalDataSource, private val remote: RemoteDataSource): ILoginRepository  {
 
     override suspend fun signUp(userCredentials: UserCredentials) = safeCall { remote.signUp(userCredentials) }
-
     override suspend fun signIn(userCredentials: UserCredentials) = safeCall { remote.signIn(userCredentials).checkIsEmailVerified() }
-
     override suspend fun resetPassword(email: String) = safeCall { remote.resetPassword(email) }
 }
